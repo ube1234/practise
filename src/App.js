bbab864dba7651359400f6ba9215c877718bae48
Sathish Lelouch <sathishsundharajan0609@gmail.com>
2025-07-03 23:10:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="pt-40 text-red-500 text-sm">
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
